b6bf8287d158a416c2225ccbf8243be7e39cddd9
syed-azhar <93089810+syed-azhar@users.noreply.github.com>
2023-08-07 20:27:03 -0700

    assignment 1

---
+++ app.js
@@ -0,0 +1,48 @@
+// 2 method of alert
+
+// alert("hello");
+// window.alert("hello1");
+
+// var fullName = "syed azhar ali";
+
+// if we update and change username
+
+// fullName ="haider"
+
+// alert(fullName)
+
+// var num = 10;
+
+// alert(num+10);
+
+// ===== Chapter 1 (Ques 1) ========
+
+// alert("hello world")
+
+// ===== Chapter 2 (Ques 2) ========
+
+// var num= 10;
+// alert(num+10)
+
+
+// ===== Chapter 3 (Ques 3) ========
+
+
+// var visits =15;
+// alert("you visited this site "+visits+"times");
+
+
+// ===== Chapter 4 (Ques 4) ========
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nZ")
+
+
+
+
+
+
+
+
+
+
+
